6956c578533af0837475393989b7a613f553f673
Sandy Ludosky <sandyludosky@gmail.com>
2020-01-11 13:24:27 +0100

    Updates

---
+++ precache-manifest.ced33bc74b37fc24bcd359fef77332ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a18046bc00e48281f682b039b4be4db5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46a413842e1aa3ce3e7f",
+    "url": "/static/css/main.fd5be9c9.chunk.css"
+  },
+  {
+    "revision": "4fa8d987e71558b8d294",
+    "url": "/static/js/2.687c3b91.chunk.js"
+  },
+  {
+    "revision": "46a413842e1aa3ce3e7f",
+    "url": "/static/js/main.0c0de419.chunk.js"
+  },
+  {
+    "revision": "634bb281097d420840d9",
+    "url": "/static/js/runtime~main.6969e643.js"
+  },
+  {
+    "revision": "cc09e1016c54311f1850fdbf70c6f381",
+    "url": "/static/media/back.cc09e101.jpg"
+  }
+]);
